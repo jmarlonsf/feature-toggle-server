cd818cda5b5f09434ab932095d5d306fdec4c842
João Marlon <marlonsouto@gmail.com>
2021-11-14 18:47:43 -0300

    firts commit

---
+++ src/main/java/com/deppit/feature/toggle/server/FeatureToggleServerApplication.java
@@ -0,0 +1,13 @@
+package com.deppit.feature.toggle.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleServerApplication.class, args);
+	}
+
+}
